a4caf224bfa513e08f4edfcc9f58762c29e63988
田口凌 <t20cs025@yamanashi.ac.jp>
2022-10-24 21:05:22 +0900

    [modify]can't move urls.py

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,25 @@
 from django.http import HttpResponse
-# Create your views here.
+from django.template import loader
+
+from django.http import Http404
+from django.shortcuts import render
+
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
+from django.template.context_processors import request
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list':latest_question_list}  
+    return render(request, 'polls/index.html',context)
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request,'polls/detail.html',{'question':question})
 
 def results(request, question_id):
     response = "you're looking at the results of question %s."
